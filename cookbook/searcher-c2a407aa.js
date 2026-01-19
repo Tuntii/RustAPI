8f1aed2dac35a1b88dc66e80385c43407775ebfd
Tuntii <Tuntii@users.noreply.github.com>
2026-01-19 01:26:12 +0000

    Deploy site + cookbook: Merge pull request #39 from Tuntii/feature/framework-ergonomics-update
    Feature/framework ergonomics update 410a7ffdfc113198d98f9b53a3741df84810aa3a

--- cookbook/searcher-c2a407aa.js
+++ cookbook/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-19cf8ce6.js',
+                path_to_root + 'searchindex-8e8b8e10.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
