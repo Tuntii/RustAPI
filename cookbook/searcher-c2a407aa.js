96902a1cde3c67a8aa6224a904257388784b1bff
Tuntii <Tuntii@users.noreply.github.com>
2026-02-02 15:23:17 +0000

    Deploy site + cookbook: Merge branch 'main' of https://github.com/Tuntii/RustAPI aacf920db1f4e99b250994b31962c02baf622fa0

--- cookbook/searcher-c2a407aa.js
+++ cookbook/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-b8492182.js',
+                path_to_root + 'searchindex-50e2da92.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
