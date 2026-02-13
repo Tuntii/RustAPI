96699b621f1e04aaf6298a477f7022b71d52b455
Tuntii <Tuntii@users.noreply.github.com>
2026-02-13 06:41:20 +0000

    Deploy site + cookbook: Merge pull request #121 from Tuntii/docs/enterprise-and-testing-14429851771554642366
    docs: Enterprise Learning Path & Testing Recipe f268d407c9535976e1e504dc8001c9c211d59094

--- cookbook/searcher-c2a407aa.js
+++ cookbook/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-6cb794e1.js',
+                path_to_root + 'searchindex-be543932.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
