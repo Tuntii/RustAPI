d9e0b7fc7c45a231ad2f29b6c81fadcb0e297bd4
Tuntii <Tuntii@users.noreply.github.com>
2026-02-15 10:21:31 +0000

    Deploy site + cookbook: Merge pull request #124 from Tuntii/docs-maintain-feb-15-8787861394030923173
    docs: Fix SSR recipe and update cookbook index fa58f531f5bd4acfe89e0a66e0ee5aa26707b78e

--- cookbook/searcher-c2a407aa.js
+++ cookbook/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-28c26c68.js',
+                path_to_root + 'searchindex-80324fc0.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
