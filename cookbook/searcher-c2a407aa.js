55ff708e9cb41c802abc60b2721ffb714c502a48
Tuntii <Tuntii@users.noreply.github.com>
2026-05-15 16:41:38 +0000

    Deploy site + cookbook: Merge pull request #176 from Tuntii/feat/embedded-isometric-dashboard
    feat(dashboard): Embedded Isometric System Dashboard 783465164156498ac945a5009186ba9437baad14

--- cookbook/searcher-c2a407aa.js
+++ cookbook/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-01cbbb90.js',
+                path_to_root + 'searchindex-bedad13c.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
